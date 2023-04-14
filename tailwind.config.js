9f9117ce215fcf7ec0724c27e7f4b6825e8f9080
4eug <debraheug@gmail.com>
2023-04-14 10:10:49 +0000

    Tailwind Config

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,32 @@ module.exports = {
     './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: '#03040E',
+        primary600: '#3F4EC4',
+      },
+      fontSize: {
+        xs: '12',
+        sm: '14px',
+        default: '16px',
+        lg: '18px',
+        h1: '56px',
+        h2: '46px',
+        h3: '38px',
+        h4: '30px',
+        h5: '24px',
+        h6: '20px',
+        core60: '60px',
+        core80: '80px',
+        core96: '96px',
+      },
+      fontWeight: {},
+      spacing: {},
+      backgroundImage: {
+        hero: "url('../public/stars.svg')",
+      },
+    },
   },
   plugins: [],
 };
