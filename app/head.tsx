fef6634f315340bdf6154c9e072ed4de96e49ba9
4eug <debraheug@gmail.com>
2023-04-14 10:11:03 +0000

    Head

---
+++ app/head.tsx
@@ -0,0 +1,12 @@
+export default function Head() {
+  return (
+    <>
+      <title>Mirage</title>
+      <meta
+        name="description"
+        content="Mirage Design System for Developers & Designers"
+      />
+      <meta name="robots" content="noindex" />
+    </>
+  );
+}
