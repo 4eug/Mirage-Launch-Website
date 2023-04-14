e97b9333ff4f6fe5456b15a7abade0920427d63d
4eug <debraheug@gmail.com>
2023-04-14 10:11:48 +0000

    Components

---
+++ components/footer.tsx
@@ -0,0 +1,11 @@
+import { Container, FooterContainer } from './container';
+
+export const Footer = () => {
+  return (
+    <footer className=" py-[5.6rem] mt-32 text-sm">
+      <Container>
+        <FooterContainer>Powered By Vercel</FooterContainer>
+      </Container>
+    </footer>
+  );
+};
---
+++ components/header.tsx
@@ -0,0 +1,41 @@
+import { Button } from './button';
+import { Container, HeaderContainer } from './container';
+import { FigmaIcon } from './icons/figma';
+import { GithubIcon } from './icons/github';
+
+import Link from 'next/link';
+import { Logo } from './icons/logo';
+
+export const Header = () => {
+  return (
+    <header className="fixed top-0 left-0 z-10 w-full backdrop-blur-[12px]">
+      <Container>
+        <HeaderContainer className="flex items-center">
+          <Link className="flex items-center text-2xl" href="/">
+            <Logo className="mr-4 h-[24px] w-[80px]" />
+          </Link>
+
+          <div className="flex-grow  flex justify-center">
+            <Button
+              className="rounded-[15px] h-[46px] gap-[5px] w-[113px] font-bold flex items-center justify-center"
+              href="/"
+            >
+              <FigmaIcon />
+              Figma
+            </Button>
+          </div>
+
+          <div className="ml-auto flex h-full items-center">
+            <Button
+              className=" h-[46px] w-[113px] flex gap-[5px] font-bold items-center justify-center bg-primary600"
+              href="/"
+            >
+              <GithubIcon />
+              Github
+            </Button>
+          </div>
+        </HeaderContainer>
+      </Container>
+    </header>
+  );
+};
