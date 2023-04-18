d862563f28359f30355a4ceb96b7db79e4d670d2
4eug <debraheug@gmail.com>
2023-04-18 09:26:31 +0000

    Animation added with framer-motion

--- components/sections/hero-section.tsx
+++ components/sections/hero-section.tsx
@@ -3,28 +3,62 @@ import { Button } from '../button';
 import { Hero, HeroTitle, HeroSubtitle } from '../hero';
 import { Ico } from '../icons/ico';
 import TimerCard from '../timer-card';
+import { motion } from 'framer-motion';
+
+let easing = [0.6, -0.05, 0.01, 0.99];
+
+const fadeInUp = {
+  initial: {
+    y: 60,
+    opacity: 0,
+    transition: { duration: 0.9, ease: easing },
+  },
+  animate: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.9,
+      ease: easing,
+    },
+  },
+};
 
 export const HomepageHero = () => (
   <>
-    <Hero>
-      <Button className="gap-x-2" href="/" variant="secondary" size="medium">
-        <Ico />
-        <span>
-          {' '}
-          Release 2023.05 | <span className="text-primary600">Coming Soon</span>
-        </span>
-      </Button>
-      <HeroTitle className="">
-        The Ultimate Design <br /> System for <br /> Devs and Designers!
-        {/* <br />
+    <motion.section
+      initial="initial"
+      animate="animate"
+      exit={{ opacity: 0 }}
+      className=""
+    >
+      <motion.div variants={fadeInUp} className="text-center">
+        <Hero>
+          <Button
+            className="gap-x-2"
+            href="/"
+            variant="secondary"
+            size="medium"
+          >
+            <Ico />
+            <span>
+              {' '}
+              Release 2023.05 |{' '}
+              <span className="text-primary600">Coming Soon</span>
+            </span>
+          </Button>
+          <HeroTitle className="">
+            The Ultimate Design <br /> System for <br /> Devs and Designers!
+            {/* <br />
         Designers! */}
-      </HeroTitle>
-      <HeroSubtitle className="">
-        Elevate your designs with Mirages extensive library of
-        <br />
-        customisable components and styles
-      </HeroSubtitle>
-      <TimerCard />
-    </Hero>
+          </HeroTitle>
+          <HeroSubtitle className="">
+            Elevate your designs with Mirages extensive library of
+            <br />
+            customisable components and styles
+          </HeroSubtitle>
+          <TimerCard />
+        </Hero>
+      </motion.div>
+    </motion.section>
   </>
 );
