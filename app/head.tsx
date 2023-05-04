44da5e834d4d4865fe1fd5ad178beca07edf7b97
4eug <debraheug@gmail.com>
2023-05-04 16:39:52 +0000

    Meta data

--- app/head.tsx
+++ app/head.tsx
@@ -7,6 +7,23 @@ export default function Head() {
         content="Mirage Design System for Developers & Designers"
       />
       <meta name="robots" content="noindex" />
+      <meta property="og:type" content="website" />
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta
+        name="twitter:image"
+        content="https://twitter.com/MirageUI/header_photo"
+      />
+      <meta property="og:url" content="https://www.mirageui.com/" />
+      <meta name="twitter:site" content="@MirageUI" />
+      <meta name="twitter:creator" content="@MirageUI" />
+      <meta
+        property="twitter:description"
+        content="The ultimate design system for developers and designers!"
+      />
+      <meta
+        name="keywords"
+        content="Design System, UI Library, Developers, Designers, Ultimate,"
+      />
     </>
   );
 }
